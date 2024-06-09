c6e80ce895c7b7a2e6cc18bb8b45f6ef8bb94684
Robhawk <robhawk97@gmail.com>
2024-06-09 10:08:19 +0200

    Update shell_app.py

--- shell_app.py
+++ shell_app.py
@@ -1,10 +1,7 @@
-
+from langchain.llms import OpenAI
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.callbacks import StreamlitCallbackHandler
 import streamlit as st
-from langchain_core.messages import AIMessage, HumanMessage
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.tools import ShellTool
 from langchain.agents import AgentType, initialize_agent
 from langchain_openai import ChatOpenAI
@@ -16,13 +13,13 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 os.environ['OPENAI_API_KEY'] = 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = 
+os.environ["LANGCHAIN_API_KEY"] =
 
 
 shell_tool = ShellTool()
-#load_dotenv()
 
-llm = ChatOpenAI()
+
+llm = ChatOpenAI(temperature=0)
 
 shell_tool.description = shell_tool.description + f"args {shell_tool.args}".replace(
     "{", "{{"
@@ -32,56 +29,10 @@ agent = initialize_agent(
 )
 
 
-# app config
-st.set_page_config(page_title="Streaming bot", page_icon="🤖")
-st.title("Streaming bot")
 
-def get_response(user_query, chat_history):
-
-    template = """
-    You are a computer networking expert and a Penetration Tester. Answer the following questions considering the history of the conversation:
-
-    Chat history: {chat_history}
-
-    User question: {user_question}
-    """
-
-    prompt = ChatPromptTemplate.from_template(template)
-
-    #llm = ChatOpenAI()
-        
-    chain = prompt | agent | StrOutputParser()
-    
-    return chain.stream({
-        "chat_history": chat_history,
-        "user_question": user_query,
-    })
-
-# session state
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [
-        AIMessage(content="Hello, I am a bot. How can I help you?"),
-    ]
-
-    
-# conversation
-for message in st.session_state.chat_history:
-    if isinstance(message, AIMessage):
-        with st.chat_message("AI"):
-            st.write(message.content)
-    elif isinstance(message, HumanMessage):
-        with st.chat_message("Human"):
-            st.write(message.content)
-
-# user input
-user_query = st.chat_input("Type your message here...")
-if user_query is not None and user_query != "":
-    st.session_state.chat_history.append(HumanMessage(content=user_query))
-
-    with st.chat_message("Human"):
-        st.markdown(user_query)
-
-    with st.chat_message("AI"):
-        response = st.write_stream(get_response(user_query, st.session_state.chat_history))
-
-    st.session_state.chat_history.append(AIMessage(content=response))
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt)
+    with st.chat_message("assistant"):
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(prompt, callbacks=[st_callback])
+        st.write(response)
